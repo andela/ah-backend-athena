1c1bed18a4ef0e7773391a9256ed8d11641e9969
joshtrigger <lugjosh@gmail.com>
2018-11-28 14:33:44 +0300

    chore(coveralls):Integrate Coveralls code coverage service
    This is to ensure that we are informed of how well the code base is being tested.
    
    [Delivers #162163259]

--- authors/settings.py
+++ authors/settings.py
@@ -78,17 +78,6 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if 'TRAVIS' in os.environ:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'athenatraviscidb',
-        'USER': 'postgres',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT': '',
-    }
-}
 
 DATABASES = {
     'default': {
