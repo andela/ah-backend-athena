a3d2f24ae0871cbe9adaabe6752760cbf60c7195
joshtrigger <lugjosh@gmail.com>
2018-12-06 22:23:55 +0300

    feat(password-reset): password reset via email link
    the user should be able to reset the password via email
    upon request.
    
    [Delivers #162163265]

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url
-from django.urls import path
+from django.urls import path, include
 
 from .views import (
-    LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
+    LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView,
+    PasswordResetView, PasswordResetConfirmView,
 )
 
 urlpatterns = [
     path('user/', UserRetrieveUpdateAPIView.as_view()),
     path('users/', RegistrationAPIView.as_view()),
     path('users/login/', LoginAPIView.as_view()),
+    path('password_reset/', PasswordResetView.as_view()),
+    url('^password_reset_confirm/(?P<slug>[-\w]+)$', PasswordResetConfirmView.as_view()),
+    # url('^password_reset_confirm/(?P<slug>[-\w]+)$', PasswordResetConfirmViewPost.as_view()),
 ]
