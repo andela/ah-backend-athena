8edad3df9ed8e6a11394c8cc03976d60179c7e4c
joshtrigger <lugjosh@gmail.com>
2018-12-07 18:16:03 +0300

    feat(password-reset): password reset via email link
    fix failing tests and add environment variables
    
    [Delivers #162163265]

--- authors/settings.py
+++ authors/settings.py
@@ -158,5 +158,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.getenv('EMAIL', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD', '')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = os.getenv('EMAIL_PORT', '')
+EMAIL_USE_TLS = os.getenv('TLS', '')
