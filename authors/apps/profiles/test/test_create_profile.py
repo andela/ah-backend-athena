245163599e83c841a8e3d26b7983a639032d7e1b
SokoPaulSokool <sopapaso73@gmail.com>
2018-12-07 14:19:16 +0300

    feat(profile): add test for geting user profile
    This commit adds tests for getting a profile of an existing user
    
    [Delivers #162163267]

---
+++ authors/apps/profiles/test/test_create_profile.py
@@ -0,0 +1,37 @@
+import json
+from django.urls import reverse
+from rest_framework.views import status
+from rest_framework.test import APITestCase, APIClient
+from ..models import Profile
+
+
+class TestProfileCreate(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.user = {
+            'user': {
+                'email': "soko@gmail.com",
+                'username': "soko",
+                'password': 'sokosoko'
+            }
+        }
+
+    def create_testing_user(self):
+        self.client.post('/api/users/', self.user, format='json')
+        response = self.client.post(
+            '/api/users/login/', self.user, format='json')
+        token = response.data['token']
+        return token
+
+    def test_get_profile(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.create_testing_user())
+        response = self.client.get('/api/profiles/soko')
+        self.assertEqual(
+            json.loads(response.content)['profile']['username'],
+            "soko"
+        )
+
+
+   
+
