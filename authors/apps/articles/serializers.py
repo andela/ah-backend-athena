da1b1612b9aa3ee9a5b5f4bb43553f6f0c17b2ea
kafuuma Henry <henry@Henry.local>
2018-12-11 16:08:25 +0300

    feat(articals): Users can create articles
    This commit delivers a feature for users to create articles
    Edit, delete functionalities are not included
    
    [Delivers #162163268]

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -1,65 +1,38 @@
 from django.contrib.auth import authenticate
 from rest_framework import serializers
 from ..authentication.models import User
+from ..profiles.serializers import ProfileSerializer
 
 from .models import(
-<<<<<<< HEAD
-    Articles,
-)
-
-
-class CreateArticalViewSerializer(serializers.ModelSerializer):
-=======
     Article,
 )
 
 
 class CreateArticleViewSerializer(serializers.ModelSerializer):
->>>>>>> feat(Articles): Users can can create articles
-    author = serializers.SerializerMethodField()
-    user_id = User.pk
-
-    def get_author(self, request):
-        author = {
-            "username": article.author.username,
-            "bio": article.author.profile.bio,
-            "image": article.author.profile.image
-        }
-        return author
+    author = ProfileSerializer(read_only=True)
 
     class Meta:
-<<<<<<< HEAD
-        model = Articles
-=======
         model = Article
->>>>>>> feat(Articles): Users can can create articles
         """
         List all of the fields that could possibly be included in a request
         or response, this includes fields specified explicitly above.
         """
-        fields = ['id', 'title', 'body', 'description',
-                  'author', 'slug', 'published', 'created_at', ]
+        fields = ['id', 'title', 'body', 'description', 'image',
+                  'author', 'slug', 'published', 'created_at']
 
         """
         Overide the validate methods to include validatiosn for 
         different fields
         """
 
-        def validate_title(self, tittle):
+        def validate_title(self, title):
             if len(title) > 200:
                 raise serializers.ValidationError(
                     'Titles are restricted to 200 characters'
                 )
 
-<<<<<<< HEAD
-        def validate_description(self.description):
-            if len(title) > 400:
-                raise serializers.ValidationError(
-                    'Descriptions are restricted to 400 characters'
-=======
         def validate_description(self, description):
             if len(title) > 400:
                 raise serializers.ValidationError(
                     'Titles are restricted to 200 characters'
->>>>>>> feat(Articles): Users can can create articles
                 )
--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -1,12 +1,10 @@
+
+import uuid
 from django.shortcuts import render
 from rest_framework import status, exceptions
 from rest_framework.generics import(
-<<<<<<< HEAD
-    CreateAPIView,
-    RetrieveUpdateAPIView
-=======
+    RetrieveUpdateAPIView,
     GenericAPIView
->>>>>>> feat(Articles): Users can can create articles
 )
 
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -15,62 +13,53 @@ from django.template.defaultfilters import slugify
 
 from ..authentication.backends import JWTAuthentication
 from ..authentication.models import User
+from .renderers import ArticleJSONRenderer
+from .models import ArticleImg, Article
+from ..profiles.models import Profile
 
 from .serializers import(
-<<<<<<< HEAD
-    CreateArticalViewSerializer,
-=======
     CreateArticleViewSerializer,
->>>>>>> feat(Articles): Users can can create articles
 
 
 )
 
 
-<<<<<<< HEAD
-class createArticalView(RetrieveUpdateAPIView):
-    permission_classes = (IsAuthenticated,)
-    # renderer_classes= (Someserializer,) Customize serializer for this app
-    serializer_class = (CreateArticalViewSerializer,)
-=======
 class createArticleView(GenericAPIView):
     permission_classes = (IsAuthenticated,)
-    # renderer_classes= (Someserializer) Customize serializer for this app
-    serializer_class = (CreateArticleViewSerializer,)
->>>>>>> feat(Articles): Users can can create articles
+    renderer_classes = (ArticleJSONRenderer,)
+    serializer_class = CreateArticleViewSerializer
 
     def post(self, request):
         """ The post method is used to create articles"""
         article = request.data.get('article', {})
         """
-        call the JWTAuthentication class to decode token 
-        and retrieve user data
+        call the JWTAuthentication class to decode token
+        and retrieve usere data
         """
-        user_info = JWTAuthentication.authenticate()
-<<<<<<< HEAD
-        article['author'] == user_detail[1]
-=======
-        article['author'] == user_infol[1]
->>>>>>> feat(Articles): Users can can create articles
+
+        image_data = article['image']
+
+        image_obj = ArticleImg(
+            image_url=image_data['image_url'],
+            description=image_data['image_description']
+        )
+        image_obj.save()
+        img_id = ArticleImg.objects.filter(
+            image_url=image_data['image_url']).first()
+        article['image'] = img_id.id
 
         """create slug from an artical"""
-        if article['slug'] == '':
-            try:
-                slug = slugify(article['title']).replace('_', '-')
-                article['slug'] = slug
-            except KeyError:
-                pass
+
+        slug = slugify(article['title']).replace('_', '-')
+        article['slug'] = slug
+
+        current_user = User.objects.all().filter(
+            email=request.user).values()[0]
+        user_id = current_user['id']
+        profile = Profile.objects.get(user__id=user_id)
 
         serializer = self.serializer_class(data=article)
         serializer.is_valid(raise_exception=True)
-<<<<<<< HEAD
-        serializer.save(author=user_info[0])
-
-        data["message"] = "Article successfully created"
+        serializer.save(author=profile)
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-=======
-        serializer.save(author=user_data[0])
-        data = serializer.data
-        data["message"] = "Article created successfully."
->>>>>>> feat(Articles): Users can can create articles
