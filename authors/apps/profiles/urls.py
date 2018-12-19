42485c58d98dc87cc3f2f180253bec61a2ed1994
KicaRonaldOkello <40671517+KicaRonaldOkello@users.noreply.github.com>
2018-12-20 00:26:15 +0300

    #162163275 List Users  (#25)

--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import include, path
 from django.conf.urls import url
-from .views import ProfileRetrieveView, FollowAPIView, FollowingAPIView, FollowersAPIView
+from .views import (ProfileRetrieveView, FollowAPIView, 
+FollowingAPIView, 
+FollowersAPIView,
+ListProfilesView)
 
 urlpatterns = [
     path('profiles/following', FollowingAPIView.as_view()),
@@ -8,4 +11,5 @@ urlpatterns = [
     url('^profiles/(?P<slug>[-\w]+)$',
         ProfileRetrieveView.as_view(), name='profiles'),
     path('profiles/<username>/follow', FollowAPIView.as_view()),
+    path('profiles/', ListProfilesView.as_view()),
 ]
