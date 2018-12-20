85bf1bf045d4fc58e8b9a1f1e0537aa74d5d17f3
NtaleGrey <Shadikntale@gmail.com>
2018-12-20 16:26:20 +0300

    bug(search functionality): fix search functionality
    - Fix search by author
    
    [Fixes  #163183529]

--- authors/settings.py
+++ authors/settings.py
@@ -144,8 +144,6 @@ CORS_ORIGIN_WHITELIST = (
 )
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ORIGIN_ALLOW_ALL = True
-
 # Tell Django about the custom `User` model we created. The string
 # `authentication.User` tells Django we are referring to the `User` model in
 # the `authentication` module. This module is registered above in a setting
