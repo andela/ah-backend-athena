9983a9b07c6939f853411abb89df06734af9479f
Ntale Shadik <39732510+Nta1e@users.noreply.github.com>
2018-12-21 12:22:06 +0300

    feat(pagination): Pagination support for articles (#24)

---
+++ authors/apps/articles/pagination.py
@@ -0,0 +1,19 @@
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.response import Response
+
+class StandardResultsPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = 'limit'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'results': data
+        })
