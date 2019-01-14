d4ad7da0d27b9b6065576e25b77b6dde869bd66f
kasulejoseph <kasule06joseph@gmail.com>
2019-01-14 10:09:49 +0300

    ch(CORS): add CORS headers
    enable cross-origin requests from frontend client
    
    [Delivers #163183420]

--- authors/settings.py
+++ authors/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'authors.urls'
 RPD = 120 #read per word
 TEMPLATES = [
